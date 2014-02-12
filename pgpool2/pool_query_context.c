47c84b1c7c06ab2c8f5a9d52441206fa283f7277
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2014-02-11 23:50:54 -0800

    ignore pool_is_failed_transaction() status if protocol version is not PROTO_MAJOR_V3

--- pgpool2/pool_query_context.c
+++ pgpool2/pool_query_context.c
@@ -705,7 +705,7 @@ void pool_where_to_send(POOL_QUERY_CONTEXT *query_context, char *query, Node *no
 				 */
 				if (TSTATE(backend, PRIMARY_NODE_ID) == 'I' ||
 					(!pool_is_writing_transaction() &&
-					 !pool_is_failed_transaction() &&
+					 !(MAJOR(backend) == PROTO_MAJOR_V3 && pool_is_failed_transaction()) &&
 					 pool_get_transaction_isolation() != POOL_SERIALIZABLE))
 				{
 					BackendInfo *bkinfo = pool_get_node_info(session_context->load_balance_node_id);
