d98bc465e13bc3360dbe930360c907e7b1510b80
Tom Zeng <tom.z.zeng@gmail.com>
2016-01-21 08:36:41 -0800

    restrict access to 'system' databases; and also merged in 'Hide temporary created pg_roles by Prestogres #37'

--- prestogres/pgsql/setup.sql
+++ prestogres/pgsql/setup.sql
@@ -36,6 +36,12 @@ begin
         security definer;
 
         revoke temporary on database "' || target_db || E'" from public;  -- reject CREATE TEMPORARY TABLE
+        revole all on database postgres from public;
+        revoke all on database template1 from public;
+        revole all on database template from public;
+        revoke select on pg_catalog.pg_roles from public;
+        revoke select on pg_catalog.pg_authid from public;
+        revoke select on pg_catalog.pg_auth_members from public;
         grant usage on schema prestogres_catalog to "' || access_role || E'";
         grant execute on all functions in schema prestogres_catalog to "' || access_role || E'";
 
